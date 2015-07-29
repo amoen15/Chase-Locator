90a9cdd7256abadabb43f65c05e29bdd466995a6
adil <adil.moen@gmail.com>
2015-07-29 17:03:31 -0400

    GPS permission, menu layout
    applied basics now

--- app/src/main/java/com/neptune/pluto/chase_locater/MainActivity.java
+++ app/src/main/java/com/neptune/pluto/chase_locater/MainActivity.java
@@ -1,90 +1,207 @@
 package com.neptune.pluto.chase_locater;
 
-import android.app.Activity;
 import android.app.Dialog;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
+import java.util.List;
 
-public class MainActivity extends Activity implements OnMapReadyCallback {
+GoogleApiClient mGoogleApiClient;
+
+public class MainActivity extends FragmentActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleMap mGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        public boolean googleServicesAvailable () {
-            int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-            if (isAvailable == ConnectionResult.SUCCESS) {
-                return true;
-            } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
-                Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, 0);
-                dialog.show();
-            } else {
-                Toast.makeText(this, "Cant connect to play services", Toast.LENGTH_LONG).show();
-            }
-            return false;
-        }
+        if (initMap()) {
+            mGoogleMap.setMyLocationEnabled(true);
 
-        if(googleServicesAvailable()) {
-            setContentView(R.layout.activity_map);
-            if(initMap()) {
+
+            if (googleServicesAvailable()) {
+                setContentView(R.layout.activity_map);
+                if (initMap()) {
+                    Toast.makeText(this, "Perfect - Maps Working", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Map not available", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                setContentView(R.layout.activity_main);
+            }
+
+
+            if (initMap()) {
                 Toast.makeText(this, "Perfect - Maps Working", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "Map not available", Toast.LENGTH_LONG).show();
+                goToLocation(38.883308, -77.015949);
             }
-        } else {
-            setContentView(R.layout.activity_main);
-        }
 
 
-
-        if(googleServicesAvailable()) {
-            Toast.makeText(this, "Perfect - Maps Working", Toast.LENGTH_LONG).show();
-            setContentView(R.layout.activity_map);
-        }
-
-        private boolean initMap(){
-            if (mGoogleMap == null) {
-                MapFragment mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-                mGoogleMap = mapFrag.getMap();
+            if (googleServicesAvailable()) {
+                Toast.makeText(this, "Perfect - Maps Working", Toast.LENGTH_LONG).show();
+                setContentView(R.layout.activity_map);
             }
-            return (mGoogleMap != null);
+
+
         }
 
+    private void goToLocation(double lat, double lng, int zoom) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
+        mGoogleMap.moveCamera(update);
 
     }
+
+    public void geoLocate(View v) throws IOException {
+
+        EditText et = (EditText) findViewById(R.id.editText1);
+        String location = et.getText().toString();
+
+        Geocoder gc = new Geocoder(this);
+        List<Address> list = gc.getFromLocationName(location, 1);
+        Address add = list.get(0);
+        String locality = add.getLocality();
+
+        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+
+        double lat = add.getLatitude();
+        double lng = add.getLongitude();
+        goToLocation(lat, lng, 15);
+
+        //mGoogleMap
+
+    }
+
+
+    private void goToLocation(double lat, double lng) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+        mGoogleMap.moveCamera(update);
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
     }
 
+    private boolean initMap() {
+        if (mGoogleMap == null) {
+            MapFragment mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+            mGoogleMap = mapFrag.getMap();
+        }
+        return (mGoogleMap != null);
+    }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    public boolean googleServicesAvailable() {
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Cant connect to play services", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    private void goToLocation(double lat, double lng) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+        mGoogleMap.moveCamera(update);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            case R.id.mapTypeNone:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+            case R.id.mapTypeNormal:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.mapTypeSatellite:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.mapTypeTerrain:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.mapTypeHybrid:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // TODO Auto-generated method stub
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000); // Update location every second
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location == null) {
+            Toast.makeText(this, "Cant get Current location", Toast.LENGTH_LONG).show();
+        } else {
+            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 15);
+            mGoogleMap.animateCamera(update);
+            setMarker("Car", "Zone", location.getLatitude(), location.getLongitude());
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 }
